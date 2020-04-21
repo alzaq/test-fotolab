4921e308311686227d1ac56107eb4329448c5fe5
alesb <ales.berger@gmail.com>
2020-04-21 16:12:34 +0200

    myapi

--- web/pages/index.js
+++ web/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Link from "next/link";
+import api from "myapi";
 
 const HomePage = () => {
   return (
@@ -10,6 +11,7 @@ const HomePage = () => {
       <Link href="/contact">
         <a>Contact Us!</a>
       </Link>
+      <pre>{JSON.stringify(api(), null, 2)}</pre>
     </>
   );
 };
